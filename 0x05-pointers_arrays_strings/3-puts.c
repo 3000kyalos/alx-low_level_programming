474747b9fbdab157badccdeb84dbdeda61c9e194
Brian <bikaloryan@gmail.com>
2024-01-02 15:14:56 +0300

    _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: string to be printed
+ * main - uses built in function
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	_puts(str);
+}

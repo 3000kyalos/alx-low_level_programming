c5b2fc669ae0705b93e77b4e3aef1c9a5db22435
Brian <bikaloryan@gmail.com>
2024-01-18 22:25:36 +0300

    correct output for 2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,47 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * str_concat - concatenates two strings
  * @s1: first string
  * @s2: second string
  *
- * Return: string, NULL on failure
+ * Return: pointer to the two strings, NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
 	char *result;
-	int s1_Len, s2_Len;
+	unsigned int i, s1_Len, s2_Len;
+
+	s1_Len = 0;
+	s2_Len = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	else
-	{
 		for (; s1[s1_Len]; s1_Len++)
 			;
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	else
-	{
 		for (; s2[s2_Len]; s2_Len++)
 			;
-	}
 	s2_Len++;
 
-	result = (char *)malloc((s1_Len + s2_Len + 1) * sizeof(char));
+	result = malloc((s1_Len + s2_Len) * sizeof(char));
 
 	if (result == NULL)
 	{
 		fprintf(stderr, "failed to allocate memory");
 		exit(1);
 	}
-	strcpy(result, s1);
-	strcat(result, s2);
+	for (i = 0; i <= (s1_Len + s2_Len); i++)
+	{
+		if (i < s1_Len)
+			result[i] = s1[i];
+		else
+			result[i] = s2[i - s1_Len];
+	}
 	return (result);
 }

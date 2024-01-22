6af21ff9730a3c36e093f1e43bfdfa9a7bf2a714
Brian <bikaloryan@gmail.com>
2024-01-23 02:51:58 +0300

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the file it was complied from
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}

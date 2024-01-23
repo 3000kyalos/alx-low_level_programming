297e5e61e1beb919c5b19af22685782c95ef595b
Brian <bikaloryan@gmail.com>
2024-01-23 04:35:24 +0300

    betty styling correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,19 @@
-typedef struct dog {
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - structure with elements name, age, owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ *
+ * Description: struct for dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
+#endif
